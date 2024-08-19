66ff9bae243c6f6472681e584e0363b361d933b1
Farid Ardiansyah <faridardiansyah061@gmail.com>
2024-08-19 15:52:56 +0700

    Files upload

---
+++ src/app/page.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Button } from '@/components/ui/button'
+const page = () => {
+  return (
+    <div>
+      hello bhai
+      <Button variant="basir" size="lg">Button</Button>
+
+    </div>
+  )
+}
+
+export default page
